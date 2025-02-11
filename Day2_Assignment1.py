a9aeaac6926b64dd7256ccd78c2ceeeaeecdd834
JyotiNhavkar <157515856+JyotiNhavkar@users.noreply.github.com>
2025-02-11 14:53:39 +0530

    Add files via upload

---
+++ Day2_Assignment1.py
@@ -0,0 +1,20 @@
+#Calculate the Multiplication and sum of two numbers
+#Input two numbers from the user
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+
+#Calculate the multiplication and sum
+multiplication = number1 * number2
+summation = number1 + number2
+
+
+#Displaying the results
+print("Multiplication is:", multiplication)
+print("Sum is:", summation)
+
+
+''' Output:
+Enter the first number: 55
+Enter the second number: 77
+Multiplication is: 4235
+Sum is: 132'''
---
+++ Day2_Assignment2.py
@@ -0,0 +1,17 @@
+ # Declare two variables and print that which variable is largest using ternary operators
+# Declare two variables
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+
+# Use ternary operator to find the largest number
+largest = a if a > b else b
+
+# Print the largest number
+print("The largest number is:", largest)
+
+
+'''Output:
+Enter the first number: 18
+Enter the second number: 90
+The largest number is: 90.0'''
+
