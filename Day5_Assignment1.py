794b7c05a0bfb4628514e2bb39ea6b2952a3c1ba
JyotiNhavkar <157515856+JyotiNhavkar@users.noreply.github.com>
2025-02-13 13:13:27 +0530

    Add files via upload

---
+++ Day5_Assignment1.py
@@ -0,0 +1,11 @@
+# To Declare a div() function with two parameters. Then call the function and pass two numbers and display their division.
+
+# function
+def div(a, b):
+    return a / b
+
+# Call the function and display result
+print("Division Is:", div(9, 3))
+
+'''Output:
+Division Is: 3.0'''
---
+++ Day5_Assignment2.py
@@ -0,0 +1,11 @@
+# To Declare a square() function with one parameter.Then call the function and pass one number and display the square of that number .
+
+# function
+def square(n):
+    return n * n
+
+# call the function and pass one number and display the square  
+print("Square:", square(80))
+
+'''Output:
+Square: 6400'''
