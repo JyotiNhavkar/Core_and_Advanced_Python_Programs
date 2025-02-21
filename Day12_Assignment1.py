031f46e20a953529981e6c4e3def24e73402bfd4
JyotiNhavkar <157515856+JyotiNhavkar@users.noreply.github.com>
2025-02-21 17:01:17 +0530

    Add files via upload

---
+++ Day12_Assignment1.py
@@ -0,0 +1,13 @@
+# Write a Python program to find the number of times 4 appears in the tuple
+
+# Defining the tuple
+tuplex = (2, 4, 5, 6, 2, 3, 4, 4, 7)
+
+# Count the occurrences of 4
+count = tuplex.count(4)
+
+# Print the result
+print("Output:", count)
+
+'''Output:
+Output: 3 '''
---
+++ Day12_Assignment2.py
@@ -0,0 +1,13 @@
+# Write a Python program to convert a list to a tuple. 
+
+# Defining the list
+listx = [5, 10, 7, 4, 15, 3]
+
+# Convert list to tuple
+tuplex = tuple(listx)
+
+# Displaying the result
+print("Output:", tuplex)
+
+'''Output:
+Output: (5, 10, 7, 4, 15, 3)'''
